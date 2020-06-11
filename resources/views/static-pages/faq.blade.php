613d03bbe117a677adc825fbf5e533e0b9bc7b46
Duc Thang Vu <vuducthang91@gmail.com>
2020-06-11 19:08:58 +0200

    content added to faq page

--- resources/views/static-pages/faq.blade.php
+++ resources/views/static-pages/faq.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.main')
 
 @section('main-content')
-    Faq page
+    <div class="container">
+        <h2>Domande frequenti</h2>
+        <p>
+            Le nostre risposte a tutte le tue domande. Non trovi la risposta che stai cercando? Contattaci e saremo felici di darti una mano.
+        </p>
+    </div>
 @endsection
