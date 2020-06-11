979978293f9e139b1b81a08c54e30cb065870b7c
Duc Thang Vu <vuducthang91@gmail.com>
2020-06-11 18:13:45 +0200

    font updated

--- resources/views/static-pages/home.blade.php
+++ resources/views/static-pages/home.blade.php
@@ -1,18 +1,20 @@
 @extends('layouts.main')
 
 @section('main-content')
-    <div class="jumbotron">
-        <div class="column">
-            <h1>Diventa sviluppatore web</h1>
-            <p>Trasformiamo la tua passione in una carriera. Se non trovi lavoro, ti rimborsiamo.</p>
-            <ul>
-                <li>6 mesi di corso intensivo online in diretta</li>
-                <li>Nessuna competenza di programmazione richiesta</li>
-                <li>Siamo certi del tuo successo, altrimenti ti rimborsiamo</li>
-            </ul>
-        </div>
-        <div class="column">
-            <img src="https://www.boolean.careers/images/homepage/pc-black-gif.gif"" alt="">
+    <div class="container">
+        <div class="jumbotron">
+            <div class="column">
+                <h1>Diventa sviluppatore web</h1>
+                <p>Trasformiamo la tua passione in una carriera. Se non trovi lavoro, ti rimborsiamo.</p>
+                <ul>
+                    <li>6 mesi di corso intensivo online in diretta</li>
+                    <li>Nessuna competenza di programmazione richiesta</li>
+                    <li>Siamo certi del tuo successo, altrimenti ti rimborsiamo</li>
+                </ul>
+            </div>
+            <div class="column">
+                <img src="https://www.boolean.careers/images/homepage/pc-black-gif.gif"" alt="">
+            </div>
         </div>
     </div>
 @endsection
