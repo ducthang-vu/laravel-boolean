52aa15df149a4518e7bd563651739a0cbdc917b4
Duc Thang Vu <vuducthang91@gmail.com>
2020-06-13 15:11:25 +0200

    progresses

---
+++ resources/views/shared/faqTemplate.blade.php
@@ -0,0 +1,11 @@
+<div class="faq-template">
+    <div class="faq-template__question">
+        <h4 class="faq-template__question__text">Chi può fare il corso Boolean?</h4>
+        <button class="faq-template__question__btn"><i class="fas fa-plus"></i></button>
+    </div>
+
+    <p class="faq-template__answer">
+        Chiunque abbia una forte curiosità verso il mondo della programmazione e voglia di imparare a fare il mestiere di sviluppatore.<br>
+        Età e curriculum non sono una discriminante: infatti, i nostri studenti hanno dai 18 ai 45 anni, sono giovani ragazzi senza alcuna esperienza lavorativa, ma anche affermati professionisti che vogliono cambiare settore.Se non hai mai programmato o se hai già seguito qualche corso ma senti di non aver approfondito abbastanza per diventare programmatore, Boolean è il corso che fa per te.
+    </p>
+</div>
--- resources/views/static-pages/faq.blade.php
+++ resources/views/static-pages/faq.blade.php
@@ -2,9 +2,24 @@
 
 @section('main-content')
     <div class="container">
-        <h2>Domande frequenti</h2>
-        <p>
-            Le nostre risposte a tutte le tue domande. Non trovi la risposta che stai cercando? Contattaci e saremo felici di darti una mano.
-        </p>
+        <section class="faq-head">
+            <h2>Domande frequenti</h2>
+            <p>
+                Le nostre risposte a tutte le tue domande. Non trovi la risposta che stai cercando? Contattaci e saremo felici di darti una mano.
+            </p>
+        </section>
+    </div>
+    <div class="faq-main">
+        <div class="container">
+            <section class="column left">
+                <h3>Prima del corso</h3>
+
+                @include('shared.faqTemplate')
+            </section>
+
+            <section class="column right">
+                <h3>Dopo il corso</h3>
+            </section>
+        </div>
     </div>
 @endsection
